39ea166116a00fcbcd5e2c16b3b2ce096f7bbb20
eric capiz <ericcapiz@gmail.com>
2020-08-06 20:22:06 -0500

    yokomo data.js started

---
+++ berkah/src/Data/Yokomo.js
@@ -0,0 +1,85 @@
+const yokomoData = [
+    {
+        id:1,
+        jis: "NS 40",
+        newJis: "32B20R",
+        containerPolypropylene: "P.P",
+        volt: 12,
+        numberOfPlateCel: 9,
+        ahCapacity20HrsRate: 32,
+        reserveCapacityMinutes: 50,
+        approxAcidVolLitres: "2.3",
+        lengthMm: 195,
+        widthMm:125,
+        heightMm:198,
+        th:220,
+        assemblyLayout: 1,
+        AcidFillingTropicalClimate27C:"1.230",
+        AcidFillingTemperateClimate27C: "1.260",
+        chargingRateAmps: "3.0"
+
+    },
+    {
+        id:2,
+        jis: "NS 40 L",
+        newJis: "32R20L",
+        containerPolypropylene: "P.P",
+        volt: 12,
+        numberOfPlateCel: 9,
+        ahCapacity20HrsRate: 32,
+        reserveCapacityMinutes: 50,
+        approxAcidVolLitres: "2.3",
+        lengthMm: 195,
+        widthMm:125,
+        heightMm:198,
+        th:220,
+        assemblyLayout: 2,
+        AcidFillingTropicalClimate27C:"1.230",
+        AcidFillingTemperateClimate27C: "1.260",
+        chargingRateAmps: "3.0"
+
+    },
+    {
+        id:3,
+        jis: "NS 40 Z",
+        newJis: "36B20R",
+        containerPolypropylene: "P.P",
+        volt: 12,
+        numberOfPlateCel: 11,
+        ahCapacity20HrsRate: 35,
+        reserveCapacityMinutes: 55,
+        approxAcidVolLitres: "2.2",
+        lengthMm: 195,
+        widthMm:125,
+        heightMm:198,
+        th:220,
+        assemblyLayout: 1,
+        AcidFillingTropicalClimate27C:"1.230",
+        AcidFillingTemperateClimate27C: "1.260",
+        chargingRateAmps: "3.0"
+
+    },
+    {
+        id:4,
+        jis: "N 40",
+        newJis: "32C24R",
+        containerPolypropylene: "P.P",
+        volt: 12,
+        numberOfPlateCel: 9,
+        ahCapacity20HrsRate: 40,
+        reserveCapacityMinutes: 60,
+        approxAcidVolLitres: "3.2",
+        lengthMm: 283,
+        widthMm: 134,
+        heightMm: 215,
+        th:223,
+        assemblyLayout: 1,
+        AcidFillingTropicalClimate27C:"1.230",
+        AcidFillingTemperateClimate27C: "1.260",
+        chargingRateAmps: "3.0"
+
+    },
+
+]
+
+export default yokomoData
