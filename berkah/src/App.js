b8600dccb760ece40491a8146c731ce90616fb77
angelynaliem <angelynaliem@gmail.com>
2020-08-09 00:40:47 -0400

    Deleted php

--- berkah/src/App.js
+++ berkah/src/App.js
@@ -5,6 +5,7 @@ import ContactForm from './Components/ContactForm';
 function App() {
   return (
     <div className="App">
+        <h1>Contact Form</h1>
          <ContactForm />
       
     </div>
