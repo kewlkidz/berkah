4ebc03e469d80135be3646f8ac0db5b0680a9cf2
eric capiz <ericcapiz@gmail.com>
2020-08-09 22:28:09 -0500

    changed folder name to client

---
+++ client/src/Data/Gforce.js
@@ -0,0 +1,44 @@
+const gForce = [
+
+      {
+          id: 1,
+          type: "JIS (Japanese Industrial Standard)",
+          description: "Full range type. Dry charge, aki dapat disimpan lebih lama. Dilengkapi dengan indikator (cat eye) dan handle. Penguapan air aki rendah sehingga mudah dalam perawatan. Memakai Polyethylene Envelope Separator dengan Cranking lebih tinggi.",
+          image: "./car_battery_placeholder.jpg"    
+      },
+      
+      {
+          id: 2,
+          type: "JASO (Japanese Automobile Standards Organization)",
+          description: "Pengembangan dari tipe JIS dimana Kapasitas dan Cranking lebih tinggi walaupun bentuk dan ukurannya sama.",
+          image: "./car_battery_placeholder.jpg" 
+       },
+       
+       {
+          id: 3,
+          type: "DIN (Deutsches Institut fur Normung)",
+          description: "Memiliki Hold Down yang khusus. Aplikasi untuk mobil mobil Eropa.",
+          image: "./car_battery_placeholder.jpg" 
+        },
+        
+        {
+          id: 4,
+          type: "Titanium",
+          description: "Aki siap pakai karena sudah diisi elektrolit. Dilengkapi dengan indikator untuk mendeteksi secara dini kondisi setrum dan dilengkapi handle. Aki calcium tanpa perlu perawatan dan penambahan air aki. Tidak direkomendasikan untuk kendaraan angkutan umum. Jenis type lengkap seperti type pada JIS, JASO, dan DIN.",
+          image: "./car_battery_placeholder.jpg" 
+        },
+        
+        {
+          id: 5,
+          type: "Heavy Duty",
+          description: "Baterai yang didesain khusus dengan plate spesial yang lebih tebal yang dapat menahan getaran/guncangan, sangat direkomendasikan untuk alat-alat berat dan kendaraan heavy duty lainnya.",
+          image: "./car_battery_placeholder.jpg" 
+        }
+        
+      ];
+      
+      
+      export default gForce;
+        
+    
+    
