305e74750d1eb1b191a5e45f05a275545253f020
angelynaliem <angelynaliem@gmail.com>
2020-08-15 23:23:57 -0400

    Updated port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.post('/api/form', (req, res) => {
     console.log(req.body)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
