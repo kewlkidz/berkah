ddaf36c9c0af8b413b786ec06d5dea0d8dbac6a9
angelynaliem <angelynaliem@gmail.com>
2020-08-15 23:57:13 -0400

    Updated index.js main

--- index.js
+++ index.js
@@ -7,10 +7,46 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.post('/api/form', (req, res) => {
-    console.log(req.body)
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+        `
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'kewlkidz110@gmail.com',
+            pass: 'helloworld123!@'
+        }
+    })
+
+    let mailOptions = {
+        from: 'test@testaccount.com',
+        to: 'kewlkidz110@gmail.com',
+        replyTo: 'test@testaccount.com',
+        subject: 'New Message',
+        text: req.body.message,
+        html: htmlEmail
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err) {
+            return console.log(err)
+        }
+        console.log('Message sent: %s', info.message)
+        console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+    })
+
+    })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
